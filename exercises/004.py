abc0fc27b50fe17ba99c89b256ffe30dbffcee11
alexfalcucc <alex.falcucci@gmail.com>
2015-01-12 15:17:05 -0200

    exercise 004 fixed

--- exercises/004.py
+++ exercises/004.py
@@ -9,3 +9,12 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
 
+def palindrome(limit):
+    results = []
+    for n in range(limit):
+        for on in range(limit):
+            results.append(str(n * on))
+    last_number = lambda x=results: max([int(n) for n in x if n == n[::-1]])
+    return last_number()
+
+print palindrome(1000)
