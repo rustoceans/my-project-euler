0494dd5075b04a3b98ec1479e9d0b6b16c27a95c
alexfalcucc <alex.falcucci@gmail.com>
2015-01-16 18:58:51 -0200

    exercise 005 fixed

--- exercises/005.py
+++ exercises/005.py
@@ -9,4 +9,19 @@ What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
 
+import sys
 
+
+def minimal_divisor(limit):
+    default = sys.maxint
+    for n in xrange(limit, default, limit):
+        if all([n % i == 0 for i in xrange(11, limit + 1)]):
+            result = n
+            break
+    return result
+
+print minimal_divisor(20)
+
+"""
+The time was 18,71s.
+"""
