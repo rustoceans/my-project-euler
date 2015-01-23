dcbb80f4fcfc29e5a4ec6f278e023ab7bee94c48
alexfalcucc <alex.falcucci@gmail.com>
2015-01-23 01:26:19 -0200

    add formula to generate and find the product for the abc variables!

--- exercises/009.py
+++ exercises/009.py
@@ -13,3 +13,21 @@ Find the product abc.
 """
 
 
+"""
+Since pythagorean triples occur as m^2 - n^2, 2mn, m^2 + n^2
+
+Therefore their sum is 2m^2 + 2mn=100
+
+m^2 + mn =500
+
+m(m+n)=500
+
+sqrt500=22.~~~
+
+therefore try m=20 , m+n=25
+n = 5
+a = 400-25=375
+b = 2*20*5=200
+c = 400+25=425
+
+"""
