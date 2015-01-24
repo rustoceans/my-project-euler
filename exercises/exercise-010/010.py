ed91b7700c3fd86ac069f5afa92ebb0d960a0ebf
alexfalcucc <alex.falcucci@gmail.com>
2015-01-23 22:33:11 -0200

    exercise 010 resolved

--- exercises/exercise-010/010.py
+++ exercises/exercise-010/010.py
@@ -8,3 +8,47 @@ Find the sum of all the primes below two million.
 """
 
 
+class Primes:
+
+    def __init__(self, limit):
+        self.limit = limit
+
+    def gen_primes(self):
+        """ Generate an infinite sequence of prime numbers.
+        """
+        # Maps composites to primes witnessing their compositeness.
+        # This is memory efficient, as the sieve is not "run forward"
+        # indefinitely, but only as long as required by the current
+        # number being tested.
+        #
+        D = {}
+
+        # The running integer that's checked for primeness
+        q = 2
+
+        while q < self.limit:
+            if q not in D:
+                # q is a new prime.
+                # Yield it and mark its first multiple that isn't
+                # already marked in previous iterations
+                #
+                yield q
+                D[q * q] = [q]
+            else:
+                # q is composite. D[q] is the list of primes that
+                # divide it. Since we've reached q, we no longer
+                # need it in the map, but we'll mark the next
+                # multiples of its witnesses to prepare for larger
+                # numbers
+                #
+                for p in D[q]:
+                    D.setdefault(p + q, []).append(p)
+                del D[q]
+
+            q += 1
+
+    def get_last_prime(self):
+        return self.gen_primes()
+
+p = Primes(2000000)
+print sum(p.get_last_prime())
