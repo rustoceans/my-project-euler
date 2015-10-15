dcf01fa404e466e2efe59f0c11772f5bc5661e91
Alexsander Falcucci <alex.falcucci@gmail.com>
2015-10-15 00:43:13 -0300

    added docstrings

--- exercises/exercise-014/014.py
+++ exercises/exercise-014/014.py
@@ -23,6 +23,9 @@ NOTE: Once the chain starts the terms are allowed to go above one million.
 
 
 def _is_even(n):
+    """
+    Just return a boolean value if the number is or isn't even.
+    """
     even = False
     if n % 2 == 0:
         even = True
@@ -30,6 +33,9 @@ def _is_even(n):
 
 
 def main(n):
+    """
+    THE MAIN FUNCTION
+    """
     return _is_even(n)
 
 
