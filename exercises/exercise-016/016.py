841079b223c56ed98c975bfe7ab0317222425a42
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-02-11 12:08:26 -0200

    moved file

---
+++ exercises/exercise-016/016.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: 016.py
+Author: Alexsander Falcucci
+Email: alex.falcucci@gmail.com
+Github: https://github.com/alexfalcucc
+Description: Resolution of 16º euler exercise
+"""
+
+
+def main():
+    """
+    Project Euler Problem 16
+    ========================
+
+    2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+    What is the sum of the digits of the number 2^1000?
+    """ 
+    pass
