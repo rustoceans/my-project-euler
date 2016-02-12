d566b7377121ec1e83ec0cbca27856626b9ab49f
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-02-12 11:14:58 -0200

    resolved 16º exercise

--- exercises/exercise-016/016.py
+++ exercises/exercise-016/016.py
@@ -9,7 +9,13 @@ Description: Resolution of 16º euler exercise
 """
 
 
-def main():
+def calc_potency(numbers):
+    """TODO: Return the multiplication at all numbers inside that list.
+    """
+    return reduce(lambda x, y: x * y, numbers)
+
+
+def main(*args):
     """
     Project Euler Problem 16
     ========================
@@ -17,5 +23,12 @@ def main():
     2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
     What is the sum of the digits of the number 2^1000?
-    """ 
-    pass
+    """
+    coeff = args[0]
+    range_base = [2] * coeff
+    mult_product = calc_potency(range_base)
+    return reduce(lambda x, y: int(x) + int(y), [n for n in str(mult_product)])
+
+
+if __name__ == "__main__":
+    print main(1000)
